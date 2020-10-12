d206d310a3879ce1e101beeb42e95da118de5b34
Joshua Medlen <joshua246@gmail.com>
2020-10-12 15:47:37 -0500

    added animals classes

---
+++ animals.py
@@ -0,0 +1,140 @@
+class Donkey:
+
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.walking = True
+        self.date_added = ""
+
+
+class Chicken:
+
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.walking = True
+        self.date_added = ""
+
+
+class Sheep:
+
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.walking = True
+        self.date_added = ""
+
+
+class Tiger:
+
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.walking = True
+        self.date_added = ""
+
+
+class Elephant:
+
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.walking = True
+        self.date_added = ""
+
+
+class Monkey:
+
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.walking = True
+        self.date_added = ""
+
+
+class Snake:
+    
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.slithering = True
+        self.date_added = ""
+
+
+class Wolf:
+
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.walking = True
+        self.date_added = ""
+
+
+class Horse:
+
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.walking = True
+        self.date_added = ""
+
+
+class Whale:
+
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.swimming = True
+        self.date_added = ""
+
+
+class Fish:
+
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.swimming = True
+        self.date_added = ""
+
+
+class Gorilla:
+
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.walking = True
+        self.date_added = ""
+
+
+class Bears:
+
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.walking = True
+        self.date_added = ""
+
+
+class Zebra:
+
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.swimming = True
+        self.date_added = ""
+
+
+class Dolphin:
+
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.swimming = True
+        self.date_added = ""
+
+
+def main():
+    harambe = Gorilla()
+    harambe.name = "Harambe"
+    harambe.species = "Baiji"
+    print(f'{harambe.name}! Species: {harambe.species} | Is {harambe.name} walking? {harambe.walking}! When was were they added? {harambe.date_added}')
