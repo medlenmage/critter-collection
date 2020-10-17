fe1887caba85cc57b3173a8d9d2d9964c2e5933e
Joshua Medlen <joshua246@gmail.com>
2020-10-17 12:25:13 -0500

    added shift property to walking animal classes

--- donkey/donkey.py
+++ donkey/donkey.py
@@ -1,7 +1,8 @@
 class Donkey:
 
-    def __init__(self):
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species, shift):
+        self.name = name
+        self.species = species
         self.walking = True
+        self.shift = shift
         self.date_added = ""
